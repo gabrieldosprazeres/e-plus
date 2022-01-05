3c360db29ee15e9b2d49cb614764e2dae3102fe7
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2022-01-05 07:52:27 -0300

    feat: creating exceptions for student

--- app/controllers/student_controller.py
+++ app/controllers/student_controller.py
@@ -1,4 +1,7 @@
 from flask import request, jsonify, current_app
+from app.controllers import check_key_for_student, check_phone, check_type_for_student
+from app.excepetions import PhoneAlreadyExistsError
+from app.excepetions.student_exception import InvalidKeyStudentError, InvalidTypeStudentError
 
 from app.models.student_model import StudentModel
 
@@ -6,9 +9,22 @@ from app.models.student_model import StudentModel
 def registering_student():
     data = request.get_json()
     
-    student = StudentModel(**data)
+    try:
+        check_key_for_student(data)
+        check_phone(data, StudentModel)
+        check_type_for_student(data)
+        student = StudentModel(**data)
     
-    current_app.db.session.add(student)
-    current_app.db.session.commit()
-    
+        current_app.db.session.add(student)
+        current_app.db.session.commit()
+
+    except (
+        InvalidKeyStudentError, 
+        InvalidTypeStudentError
+        ) as error:
+        return jsonify(error.message), 400
+
+    except PhoneAlreadyExistsError as error:
+        return jsonify(error.message), 409
+
     return jsonify(student), 201
