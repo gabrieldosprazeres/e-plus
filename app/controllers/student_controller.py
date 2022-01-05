cbdee205e8316e7387a2069208e4f272ab1432d5
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2022-01-05 08:41:23 -0300

    feat: creating get_all_students route

--- app/controllers/student_controller.py
+++ app/controllers/student_controller.py
@@ -2,8 +2,14 @@ from flask import request, jsonify, current_app
 from app.controllers import check_key_for_student, check_phone, check_type_for_student
 from app.excepetions import PhoneAlreadyExistsError
 from app.excepetions.student_exception import InvalidKeyStudentError, InvalidTypeStudentError
-
 from app.models.student_model import StudentModel
+
+
+def get_all_students():
+    
+    students = StudentModel.query.all()
+    
+    return jsonify(students), 200
 
 
 def registering_student():
--- app/routes/student_blueprint.py
+++ app/routes/student_blueprint.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
-from app.controllers.student_controller import registering_student
+from app.controllers.student_controller import get_all_students, registering_student
 
 
-bp_student = Blueprint('bp_student', __name__, url_prefix='/student')
+bp_student = Blueprint('bp_student', __name__, url_prefix='/students')
 
+
+bp_student.get('')(get_all_students)
 bp_student.post('')(registering_student)
