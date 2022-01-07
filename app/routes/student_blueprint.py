7be2c8e4de788520456f5bff5c06a6d909996a70
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2022-01-07 07:28:51 -0300

    feat: creating login_controller

--- app/routes/student_blueprint.py
+++ app/routes/student_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from app.controllers.student_controller import get_all_students, registering_student
 from app.controllers.phone_controller import creating_phone_number
+from app.controllers.login_controller import signin_student
 
 
 bp_student = Blueprint('bp_student', __name__, url_prefix='/students')
@@ -9,3 +10,4 @@ bp_student = Blueprint('bp_student', __name__, url_prefix='/students')
 bp_student.get('')(get_all_students)
 bp_student.post('')(registering_student)
 bp_student.post('/phone/<int:student_id>')(creating_phone_number)
+bp_student.post('/signin')(signin_student)
