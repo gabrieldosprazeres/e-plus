11f0a2da539d3ab48d29b68df4917714ef3aa1fb
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2022-01-08 11:24:28 -0300

    fix: adjusting some details

--- app/routes/student_blueprint.py
+++ app/routes/student_blueprint.py
@@ -5,15 +5,15 @@ from app.controllers.login_controller import signin_student
 from flask_jwt_extended import jwt_required
 
 bp_student = Blueprint('bp_student', __name__, url_prefix='/students')
-
-bp_student.get('')(get_all_students)
-bp_student.get('/student/<int:student_id>')(get_filter_by_id)
-bp_student.get('/student')(jwt_required()(get_info_student))
-bp_student.post('/student/signup')(registering_student)
-bp_student.post('/phone')(jwt_required()(creating_phone_number))
-bp_student.post('/student/signin')(signin_student)
-bp_student.patch('/phone/updating/<int:phone_id>')(jwt_required()(updating_phone))
-bp_student.patch('/student/updating')(jwt_required()(updating_student))
-bp_student.patch('/student/address/updating')(jwt_required()(updating_address))
-bp_student.delete('/phone/deleting/<int:phone_id>')(jwt_required()(deleting_phone))
-bp_student.delete('/student/deleting')(jwt_required()(deleting_student))
+																							# Student CRUD
+bp_student.get('')(get_all_students)														# List all registered students
+bp_student.get('/student')(jwt_required()(get_info_student))								# Lists the information of the student who is logged in (token required)
+bp_student.get('/student/<int:student_id>')(get_filter_by_id)								# Filters a student by their ID
+bp_student.post('/student/signup')(registering_student)										# Register a student on the API
+bp_student.post('/student/signin')(signin_student)											# Login to the API
+bp_student.patch('/student/updating')(jwt_required()(updating_student))						# Update student data in API
+bp_student.patch('/student/address/updating')(jwt_required()(updating_address))				# Update student address data in API
+bp_student.delete('/student/deleting')(jwt_required()(deleting_student))					# Delete a student from the API
+bp_student.post('/student/phone')(jwt_required()(creating_phone_number))					# Register a phone number for the student
+bp_student.patch('/student/phone/updating/<int:phone_id>')(jwt_required()(updating_phone))	# Update a phone number on the student
+bp_student.delete('/student/phone/deleting/<int:phone_id>')(jwt_required()(deleting_phone))	# Deletes a phone number on the student
--- app/routes/subject_blueprint.py
+++ app/routes/subject_blueprint.py
@@ -5,6 +5,6 @@ from app.controllers.student_subject_controller import student_registration_subj
 
 bp_subject = Blueprint('bp_subject', __name__, url_prefix='/subjects')
 
-bp_subject.get('/get_subject_by_id/<int:subject_id>')(get_subject_by_id)
-bp_subject.post('')(creating_subject)
-bp_subject.post('/registration')(jwt_required()(student_registration_subject))
+bp_subject.post('')(creating_subject)															# Create a discipline
+bp_subject.get('/subject/<int:subject_id>')(get_subject_by_id)									# Get a specific course with all enrolled students
+bp_subject.post('/subject/student/registration')(jwt_required()(student_registration_subject))	# Enrolls a student in the specific subject
